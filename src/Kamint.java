5df51736645ec16f7a0a07da7765d7030750b1e9
obidkon <obid.sharipov.80@mail.ru>
2020-11-07 16:01:13 +0500

    comint

---
+++ src/Kamint.java
@@ -0,0 +1,14 @@
+public class Kamint {
+    public static void main(String[] args) {
+        System.out.println("a b c 123");
+         System.out.print("bu\"baliq\" ko'k edi'");
+
+        System.out.println("These are double quets \"\"");
+        System.out.println("This is backslash\\");
+        System.out.println("\"stinki\" nima degani ");
+        System.out.println(" The tile is located at ~\\famlyCode\\");
+        System.out.println("This is a tab\t!");
+        System.out.println(" Tis \n is \n on \n mutyply lines");
+
+    }
+}
